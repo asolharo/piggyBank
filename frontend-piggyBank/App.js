ff94815ce0c539e24167cf2914b44167fe0e14b9
fedpre <fed.pregnolato@gmail.com>
2022-05-24 10:10:06 +0200

    Navigation initialized with @react-navigation

--- frontend-piggyBank/App.js
+++ frontend-piggyBank/App.js
@@ -1,11 +1,38 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import Login from './screens/Login';
+import Dashboard from './screens/Dashboard';
 
+// Initialize the navigation object
+const Stack = createStackNavigator();
+
+// Create the default theme for the app
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    backgroud: 'transparent', 
+  }
+}
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to our Piggy Bank app!</Text>
-
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouteName="Login">
+        <Stack.Screen 
+          name='Login'
+          component={Login}
+        />
+        <Stack.Screen 
+          name='Dashboard'
+          component={Dashboard}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
+export default App;

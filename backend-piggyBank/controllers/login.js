72523be08fbb28f1ba975544e97cdfc5de3b85ac
Ashley Zufelt <ashley.zufelt@gmail.com>
2022-05-30 09:55:54 -0400

    Ashley added user routes, database connection

---
+++ backend-piggyBank/controllers/login.js
@@ -0,0 +1,220 @@
+const User = require('../models/user');
+
+const {
+  validationResult
+} = require('express-validator');
+
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
+// POST add new user
+exports.postAddUser = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed.');
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
+
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+  const username = req.body.username;
+  const password = req.body.password;
+  bcrypt
+    .hash(password, 12)
+    .then(hashedPw => {
+      const user = new User({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        username: username,
+        password: hashedPw
+      });
+      return user.save();
+
+    })
+
+    .then(result => {
+      res.status(201).json({
+        message: 'User created!',
+        userId: result._id
+      });
+
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+
+};
+
+// GET user by ID
+exports.getUserById = (req, res, next) => {
+  User.findById(req.params.userId)
+    .then(user => {
+      res.send(user);
+    })
+    .catch(error => {
+      return res.status(500).send(error);
+    });
+}
+
+// PUT update user by ID
+exports.getUserById = (req, res, next) => {
+  User.findById(req.params.userId)
+    .then(user => {
+      res.send(user);
+    })
+    .catch(error => {
+      return res.status(500).send(error);
+    });
+}
+
+//PUT user by ID
+exports.putUpdateUser = (req, res, next) => {
+  const authUserId = req.userId;
+  const userId = req.params.userId;
+
+  //Authorize user
+  if (userId !== authUserId) {
+    const error = new Error('User not authorized');
+    error.statusCode = 403;
+    throw error;
+  }
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed.');
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+  const username = req.body.username;
+  const password = req.body.password;
+
+
+  User.findById(userId)
+    .then(user => {
+      if (!user) {
+        const error = new Error('Could not find user');
+        error.statusCode = 404;
+        throw error;
+      }
+      bcrypt
+        .hash(password, 12)
+        .then(hashedPw => {
+          user.firstName = firstName;
+          user.lastName = lastName;
+          user.email = email;
+          user.username = username;
+          user.password = hashedPw;
+          return user.save();
+        });
+    })
+    .then(result => {
+      res.status(200).send(result);
+    })
+    .catch(error => {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+      next(error);
+    })
+};
+
+// DELETE user by ID
+exports.deleteUser = (req, res, next) => {
+  const authUserId = req.userId;
+  const userId = req.params.userId;
+
+  //Authorize user
+  if (userId !== authUserId) {
+    const error = new Error('User not authorized');
+    error.statusCode = 403;
+    throw error;
+  }
+
+  User.findById(req.params.userId)
+    .then(user => {
+      if (!user) {
+        const error = new Error('Could not find User.');
+        console.log("No user found")
+        error.statusCode = 404;
+        throw error;
+      }
+
+
+      return User.findByIdAndRemove(req.params.userId);
+
+    })
+    .then(result => {
+      console.log(result);
+      res.status(200).json({
+        message: 'Deleted user. '
+      });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+
+};
+
+//POST login User
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let loadedUser;
+
+  User.findOne({
+      email: email
+    })
+    .then(user => {
+
+      if (!user) {
+        const error = new Error(' Username or password were invalid');
+        error.statusCode = 401;
+        throw error;
+      }
+      loadedUser = user;
+
+      return bcrypt.compare(password, user.password);
+    })
+    .then(isEqual => {
+
+      if (!isEqual) {
+        const error = new Error('Wrong password or username')
+        error.statusCode = 401;
+        throw error;
+      }
+      const token = jwt.sign({
+        email: loadedUser.email,
+        userId: loadedUser._id.toString()
+      }, process.env.SECRET_STRING, {
+        expiresIn: '1h'
+      });
+
+      res.header('Authorization', 'Bearer ' + token);
+
+      res.status(200).json({
+        token: token,
+        userId: loadedUser._id.toString()
+      });
+
+    }).catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
--- backend-piggyBank/index.js
+++ backend-piggyBank/index.js
@@ -1,15 +1,72 @@
-const express = require('express');
-const testRouter = require('./controllers/test');
 require('dotenv').config()
+const cors = require('cors');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const session = require('express-session');
+const MongoDB = require('connect-mongodb-session')(session);
+const mongoose = require('mongoose');
+
+
+// routes
+// const testRouter = require('./controllers/test');
+const userRoute = require('./routes/login');
+
+// connect to database
+const MONGODB_URI = process.env.MONGODB_URI;
+const PORT = process.env.port || 3000;
+
+// initialize object in database
+const db = new MongoDB({
+  uri: MONGODB_URI,
+  user: []
+});
 
 // create instance of express server
 const app = express();
+
+//Swagger set up to test database endpoints
+
+// allow requests
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods', 'OPTIONS,GET,POST,PUT,DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Header', 'Content-Type, Authorization');
+  next();
+})
+
+// access in routes
+app.db = db;
+
 // add the middleware to accept json HTTP requests
 app.use(express.json());
+app.use(bodyParser.json());
 
+// Using the routes as defined
+app.use('/user', userRoute);
 // test route endpoint
-app.use('/ping', testRouter)
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+// app.use('/ping', testRouter)
+app.use((err, req, res, next) => {
+  console.log(error);
+  const message = error.statusCode || 500;
+  const data = error.data;
+  res.status(status).json({
+    message: message,
+    data: data
+  });
 });
+
+
+// connect to database
+mongoose.connect(MONGODB_URI)
+  .then(result => {
+    app.listen(PORT);
+    console.log(`Successful connection to database`);
+    console.log(`Server running on port ${process.env.PORT}`);
+  })
+  .catch(err => {
+    console.log(err);
+  })

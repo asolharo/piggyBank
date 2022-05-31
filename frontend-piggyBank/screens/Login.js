351095f8f0e508ea261dcda9005b1a8048e026f6
Angel Solano <solanoangel@icloud.com>
2022-05-31 08:48:38 -0700

    Login screen input fields added

--- frontend-piggyBank/screens/Login.js
+++ frontend-piggyBank/screens/Login.js
@@ -1,10 +1,62 @@
-import { Text, View, SafeAreaView, TextInput } from 'react-native';
-import AppBar from '../components/AppBar';
+import React from 'react';
+import { Text, View, TextInput, Button, StyleSheet } from 'react-native';
 
-const Login = () => {
+const LoginScreen = ({ navigation }) => {
+
   return (
-    <Text>Login</Text>
-  )
-}
+    <View style={styles.container}>
+      <View>
+        <Text>Username</Text>
+        <TextInput
+          style={styles.input}
+          //onChangeText={(text) => onChangeEmail(text)}
+          placeholder="Enter your username"
+          placeholderTextColor="lightgray"
+        />
+      </View>
+      <View style={styles.fields}>
+        <Text>Password</Text>
+        <TextInput
+          style={styles.input}
+          //onChangeText={(text) => onChangePassword(text)}
+          placeholder="Enter your password"
+          placeholderTextColor="lightgray"
+          secureTextEntry
+        />
+      </View>
+      <Button
+        title="Log in"
+        onPress={() => navigation.navigate('Home')}
+      />
+      <View style={{flexDirection: 'row'}}>
+        <Text>Don't have an account? </Text>
+        <Button
+          title="Sign Up"
+          onPress={() => navigation.navigate('CreateAccount')}
+        />
+      </View>
+    </View>
+  );
+};
 
-export default Login;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    width: 300,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginTop: 0,
+    paddingLeft: 8,
+    borderRadius: 30,
+  },
+  fields: {
+    marginTop: 20,
+  }
+});
+
+export default LoginScreen;
